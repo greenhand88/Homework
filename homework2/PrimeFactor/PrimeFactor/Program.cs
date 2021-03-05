c29d5c12a8113ef40394096c4f6cb700dfbe50b3
greenhand88 <54199684+greenhand88@users.noreply.github.com>
2021-03-05 02:12:09 -0600

    Add files via upload

---
+++ homework2/PrimeFactor/PrimeFactor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 10;//This is for test
+            FindPrimeFactor(test);
+            Console.Read();//For pause
+        }
+        public static void FindPrimeFactor(int target)
+        {   int temp = target;
+            int[] list = new int[1000];
+            int count = 0;
+            for(int i = 2; i <=temp; i++)
+            {
+                if (temp % i == 0&&count<32)
+                {
+                    temp = temp / i;
+                    list[count] = i; 
+                    i = 2;
+                    count++;
+                }
+            }
+            if(count<1000)
+                for (int i = 0; i <count; i++)
+                    Console.Write(list[i].ToString() + ' ');
+        }
+    }
+}
