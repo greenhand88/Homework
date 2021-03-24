e57463e6b9f72e58c23700431c080340f9b4d5a7
greenhand88 <54199684+greenhand88@users.noreply.github.com>
2021-03-24 10:14:05 +0800

    Add files via upload

---
+++ homework4/homework1/homework1/GenericList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    
+    public class GenericList<T> where T : struct, IComparable<T>
+    {
+
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        } 
+        public void ForEach(Action<Node<T>, double[]> action, ref double[]record)
+        {
+            if (head != null)
+            {
+                Node<T> temp = head;
+                while (temp != null)
+                {
+                    action(temp,record);
+                    temp = temp.Next;
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("链表为空");
+            }
+        }
+        
+    }
+}
---
+++ homework4/homework1/homework1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{/*1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+  * 通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。*/
+    
+    public class Test
+    {
+        
+        public static void  Main(string[] args)
+        {
+            double[] collection = { 0,0,0 };
+            double[] temp = { 1.5, 2.6, 3.8 };
+            GenericList<double> a=new GenericList<double>();
+            foreach(double i in temp)
+            {
+                a.Add(i);
+            }
+            Action<Node<double>, double[]> action;
+            action=(s,record) =>
+            {
+                Console.Write(s.Data + "  ");
+                if (s.Data > record[0])
+                    record[0] = s.Data;
+                if (s.Data < record[1])
+                    record[1] = s.Data;
+                record[2] += s.Data;
+            };
+            a.ForEach(action,ref collection);
+            Console.WriteLine();
+            Console.WriteLine("MAX:" + collection[0]);
+            Console.WriteLine("MIN:" + collection[1]);
+            Console.WriteLine("SUM:" + collection[2]);
+            Console.Read();
+        }
+
+    }
+    
+ 
+}
